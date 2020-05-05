454865823254b08da360761d61ebd19c95eb5a7d
Anthony Thillerot <anthony.thillerot@gmail.com>
2020-05-05 13:46:47 +0200

    new function and all comment line and docstring

--- classes.py
+++ classes.py
@@ -7,75 +7,95 @@ Created on 27 avr 2020
 from random import randint
 from random import seed
 from time import time
-
-# CONSTANT
-NUMBER_OF_SPRITEX = 15
-NUMBER_OF_SPRITEY = 15
+import constant as C
 
 
 class MacGyverClass:
+    """
+    class of mac gyver object, the class contain function,
+    Get position in sprite
+    Test if the move is possible
+    """
+
     def __init__(self, position_in_sprites):
         self.position_x = 0
         self.position_y = 0
-        self.number_of_sprite_x = NUMBER_OF_SPRITEX
-        self.number_of_sprite_y = NUMBER_OF_SPRITEY
+        self.number_of_sprite_x = C.NB_SPRITE_X
+        self.number_of_sprite_y = C.NB_SPRITE_Y
         self.max_sprites = self.number_of_sprite_x * self.number_of_sprite_y
         assert 1 <= position_in_sprites <= self.max_sprites
         self.position_in_sprites = position_in_sprites
         self.map = []
 
     @property
-    def GetPositionInSprites(self):
-        """ Get Position In Sprites"""
+    def setposition_in_sprites(self):
+        """
+        Get Position In Sprites
+        """
         return self.position_in_sprites
 
     @property
-    def GetMap(self):
-        """ Get map"""
+    def get_map(self):
+        """
+        Get map list
+        """
         return self.map
 
-    def PossibleMove(self, mov, mapjson_select):
-        """ test if the movemennt is possible and return true or false"""
+    def possible_move(self, mov, mapjson_select):
+        """
+        test if the movemennt is possible and return true or false
+        """
         self.map = mapjson_select
         pos = self.position_in_sprites
+        # position_in_sprites in range 1 to NB_MAX_SPRITE
+        # not 0 to NB_MAX_SPRITE - 1
         nb_sprites_x = self.number_of_sprite_x
-        nb_sprites_y = self.number_of_sprite_x
+        nb_sprites_y = self.number_of_sprite_y
 
         if mov == "RIGHT":
             if pos % nb_sprites_x == 0 or mapjson_select[pos] == "W":
+                # return false if we are on a border
+                # or the desired position is equal to W
                 return False
-            else:
-                self.map[self.position_in_sprites-1] = "N"
-                self.map[self.position_in_sprites] = "M"
-                self.position_in_sprites = self.position_in_sprites + 1
+            self.map[self.position_in_sprites-1] = "N"
+            self.map[self.position_in_sprites] = "M"
+            self.position_in_sprites = self.position_in_sprites + 1
         elif mov == "LEFT":
             if pos % nb_sprites_x == 1 or mapjson_select[pos-2] == "W":
+                # return false if we are on a border
+                # or the desired position is equal to W
                 return False
-            else:
-                self.map[self.position_in_sprites-1] = "N"
-                self.map[self.position_in_sprites - 2] = "M"
-                self.position_in_sprites = self.position_in_sprites - 1
+            self.map[self.position_in_sprites-1] = "N"
+            self.map[self.position_in_sprites - 2] = "M"
+            self.position_in_sprites = self.position_in_sprites - 1
         elif mov == "UP":
-            if pos / 15 - 1 <= 0 or mapjson_select[pos - 16] == "W":
+            if (pos / C.NB_SPRITE_X ) - 1 <= 0 or mapjson_select[pos - (C.NB_SPRITE_X + 1) ] == "W":
+                # return false if we are on a border
+                # or the desired position is equal to W
                 return False
-            else:
-                self.map[self.position_in_sprites-1] = "N"
-                self.map[self.position_in_sprites - 16] = "M"
-                self.position_in_sprites = self.position_in_sprites - 15
+            self.map[self.position_in_sprites - 1] = "N"
+            self.map[self.position_in_sprites - (C.NB_SPRITE_X + 1)] = "M"
+            self.position_in_sprites = self.position_in_sprites - C.NB_SPRITE_X
         elif mov == "DOWN":
-            if pos / 15 + 1 > nb_sprites_y or mapjson_select[pos + 14] == "W":
+            if (pos / C.NB_SPRITE_X) + 1 > nb_sprites_y or mapjson_select[pos + (C.NB_SPRITE_X - 1)] == "W":
+                # return false if we are on a border
+                # or the desired position is equal to W
                 return False
-            else:
-                self.map[self.position_in_sprites-1] = "N"
-                self.map[self.position_in_sprites + 14] = "M"
-                self.position_in_sprites = self.position_in_sprites + 15
+            self.map[self.position_in_sprites-1] = "N"
+            self.map[self.position_in_sprites + (C.NB_SPRITE_X - 1)] = "M"
+            self.position_in_sprites = self.position_in_sprites + C.NB_SPRITE_X
         return True
 
 
 class ObjetInMap:
+    """
+    This class make a ocject in the list of element (map_select) and this
+    caracteristique
+    """
+
     def __init__(self, position_in_sprites):
-        self.number_of_sprite_x = NUMBER_OF_SPRITEX
-        self.number_of_sprite_y = NUMBER_OF_SPRITEY
+        self.number_of_sprite_x = C.NB_SPRITE_X
+        self.number_of_sprite_y = C.NB_SPRITE_Y
         self.max_sprites = self.number_of_sprite_x * self.number_of_sprite_y
         assert 1 <= position_in_sprites <= self.max_sprites
         self.position_in_sprites = position_in_sprites
@@ -86,15 +106,30 @@ class ObjetInMap:
         self.objet_caractere = ""
 
     @property
-    def GetPositionInSprites(self):
-        """ Get Position In Sprites"""
+    def get_position_in_sprites(self):
+        """
+        Get Position In Sprites
+        """
         return self.position_in_sprites
 
-    def SetPositionRandom(self, map_select):
-        Nb_N_In_Mapcount = int(map_select.count('N'))
+    def set_position_random(self, map_select):
+        """
+        This function set a random position for the object created
+        in the map where the element egal N (nothink)
+        Parameters
+        ----------
+        map_select : TYPE = list of element in map
+            DESCRIPTION.
+
+        Returns
+        -------
+        map_select : TYPE = = list of element in map
+            DESCRIPTION.
+
+        """
         seed(time()+self.position_in_sprites)
-        number_rnd = randint(4, Nb_N_In_Mapcount)
-        print(number_rnd)
+        number_rnd = randint(4, int(map_select.count('N')))
+        # print(number_rnd)
         assert 1 <= number_rnd <= self.max_sprites
         nb_n_in_map = 0
 
--- constant.py
+++ constant.py
@@ -3,11 +3,21 @@
 """
 Created on 27 avr 2020
 @author: anthony thillerot
+
+This file contains all the game constant
+
 """
 
 # Constant
-NB_PIXEL_X = 480
-NB_PIXEL_Y = 512
+NB_PIX_SPRITE = 32
+NB_SPRITE_X = 15
+NB_SPRITE_Y = NB_SPRITE_X
+NB_MAX_SPRITE = NB_SPRITE_X * NB_SPRITE_Y
+NB_PIXEL_X = NB_SPRITE_X * NB_PIX_SPRITE
+NB_PIXEL_Y = NB_SPRITE_X * NB_PIX_SPRITE + NB_PIX_SPRITE
+
+NB_RD_IN_BRANCH = int(NB_SPRITE_X / 15) * 50
+
 PATH_OF_MUSIC = "ressource/bensound-betterdays.wav"
 PATH_OF_MACGYVER = "ressource/MacGyver.png"
 PATH_OF_WALL = "ressource/wall_picture.png"
